bc986b5b6b40c22fe39fc885cb9dfc5d157ad423
vecteurNabla <nicolas.nardino@club-internet.fr>
2020-06-04 15:39:46 +0200

    Added documentation

--- fibheap.mli
+++ fibheap.mli
@@ -1,16 +1,16 @@
-type 'a fibTree = Node of 'a * ('a fibTree) Cdlist.cdlist ref
-
-val (</) : ('a * 'b) fibTree -> ('c * 'b) fibTree -> bool
-
-type fibHeap = {
-    mutable min : (int * float) fibTree Cdlist.cdlist;
-    degs : (int * float) fibTree Cdlist.cdlist list array;
-  }
-
+type fibHeap
+(** The type for Fibonacci heaps. *)
+   
 val make : unit -> fibHeap
-
+(** Creates a new, empty Fibonnaci heap. *)
+  
 val add : int -> float -> fibHeap -> unit
-
+(** [add e p f] adds an element of key [e] with priority [p] to the heap [f]. *)
+  
 val merge : fibHeap -> fibHeap -> fibHeap
-
+(** [merge f f'] merges the two heaps. *)
+  
 val extract_min : fibHeap -> int * float
+(** [extract_min f] returns the element with the lowest priority,
+ ** along with its priority, and deletes it from f. *)
+                                     
